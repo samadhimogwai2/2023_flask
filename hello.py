c1284830a8ff2a934da7516193c3fb76743b3b40
Jona <87903362+samadhimogwai2@users.noreply.github.com>
2023-11-04 13:53:10 +0900

    render_template_1

--- hello.py
+++ hello.py
@@ -1,8 +1,13 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
-@app.route("/japan/<city>")
-def japan(city):
-    return f"Hello, {city}, in Japan!"
+# @app.route("/japan/<city>")
+# def japan(city):
+#     return f"Hello, {city}, in Japan!"
+
+@app.route("/")
+def hello():
+    return render_template('hello.html')
 
